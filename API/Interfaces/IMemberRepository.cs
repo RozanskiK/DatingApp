eaa525abdd02a0848f03cbdeb32f49ae03da561b
RozanskiK <kacroz891@gmail.com>
2026-04-11 16:36:32 +0200

    End of Section 8

---
+++ API/Interfaces/IMemberRepository.cs
@@ -0,0 +1,11 @@
+using System;
+using API.Entities;
+
+public interface IMemberRepository
+{
+    void Update(Member member);
+    Task<bool> SaveAllAsync();
+    Task<IReadOnlyList<Member>> GetMembersAsync();
+    Task<Member?> GetMemberByIdAsync(string id);
+    Task<IReadOnlyList<Photo>> GetPhotosForMemberAsync(string memberId);
+}
